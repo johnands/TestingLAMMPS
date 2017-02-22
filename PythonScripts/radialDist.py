767f4e052f11f2587b7ac643a9d9041a67cead6e
John-Anders Stende <jastende@gmail.com>
2017-02-22 11:14:54 +0100

    Radial distribution sw Silicon

--- PythonScripts/radialDist.py
+++ PythonScripts/radialDist.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('../Argon/Data/radialDist.dat', 'r') as inFile:
+with open('../Silicon/Data/radialDist.dat', 'r') as inFile:
 	
 	# skip lines
     for _ in range(3):
@@ -10,15 +10,29 @@ with open('../Argon/Data/radialDist.dat', 'r') as inFile:
     timeStep, nBins = inFile.readline().split()
     timeStep = float(timeStep); nBins = float(nBins)
     
-    bins = []; bins.append(0)
+    bins = []; bins.append(0.0)
     distribution = []
     for line in inFile:
         words = line.split()
+        if len(words) != 4:
+			break
         bins.append(float(words[1]))
         distribution.append(float(words[2]))
 
+	i = 0
+    for line in inFile:
+        words = line.split()
+        print words
+        if len(words) != 4:
+            i = 0
+            continue
+        distribution[i] += float(words[2])
+        i += 1
+		
+
+
 bins = np.array(bins)
-distribution = np.array(distribution)
+distribution = np.array(distribution) / 200
 binCenters = (bins[1:] + bins[:-1]) / 2.0
 
 plt.plot(binCenters, distribution)
