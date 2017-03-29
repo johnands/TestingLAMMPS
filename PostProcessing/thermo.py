0db14b91e744fc61661e50ff9f9d3d295b235430
John-Anders Stende <jastende@gmail.com>
2017-03-30 01:51:56 +0200

    Plotting energies

--- PostProcessing/thermo.py
+++ PostProcessing/thermo.py
@@ -29,22 +29,24 @@ def readFile(filename):
     return temperature, kineticEnergy, potentialEnergy, pressure
     
 
-tempNN, kinNN, potNN, pressNN = readFile('../TestNN/Data/Thermo/21.03-14.16.57/thermo.txt')
-tempSW, kinSW, potSW, pressSW = readFile('../Silicon/Data/Thermo/21.03-14.05.53/thermo.txt')
+tempNN, kinNN, potNN, pressNN = readFile('../TestNN/Data/Thermo/thermo.txt')
+tempSW = kinSW = potSW = pressSW = np.zeros(len(tempNN))#readFile('../Silicon/Data/Thermo/21.03-14.05.53/thermo.txt')
 
 totalEnergyNN = kinNN + potNN
 totalEnergySW = kinSW + potSW
 
+print (totalEnergyNN[-1] - totalEnergyNN[0])/totalEnergyNN[0]
+print potNN[-1] - potNN[0]
 # compute averages
 #aveTempNN = np.sum(tempNN) / 
 
 plt.subplot(2,2,1)
-plt.plot(tempNN, 'b-', tempSW, 'g-')
+plt.plot(tempNN)#, 'b-', tempSW, 'g-')
 plt.subplot(2,2,2)
-plt.plot(kinNN, 'b-', kinSW, 'g-')
+plt.plot(kinNN)#, 'b-', kinSW, 'g-')
 plt.subplot(2,2,3)
-plt.plot(potNN, 'b-', potSW, 'g-')
+plt.plot(potNN)#, 'b-', potSW, 'g-')
 plt.subplot(2,2,4)
-plt.plot(totalEnergyNN, 'b-', totalEnergySW, 'g-')
+plt.plot(totalEnergyNN)#, 'b-', totalEnergySW, 'g-')
 plt.show()
 
