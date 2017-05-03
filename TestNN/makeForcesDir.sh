791c8b8105d4bcdd74f00814dacdf7360e720831
johnands <jastende@gmail.com>
2017-05-03 21:24:45 +0200

    .

--- TestNN/makeForcesDir.sh
+++ TestNN/makeForcesDir.sh
@@ -4,9 +4,9 @@ DATE=`date +%d.%m-%H.%M.%S`
 dir="Data/Forces/$DATE"
 mkdir $dir
 
-move1="log.lammps $dir"
+copy1="log.lammps $dir"
 move2="Data/Forces/forces.txt $dir"
-copy1="../../lammps/src/pair_nn_angular2.cpp $dir"
-mv $move1
-mv $move2
+copy2="../../lammps/src/pair_nn_angular2.cpp $dir"
 cp $copy1
+mv $move2
+cp $copy2
--- TestNN/makeThermoDir.sh
+++ TestNN/makeThermoDir.sh
@@ -4,14 +4,12 @@ DATE=`date +%d.%m-%H.%M.%S`
 dir="Data/Thermo/$DATE"
 mkdir $dir
 
-move1="log.lammps $dir"
-#move2="Data/Thermo/pairForces.txt $dir"
-#move3="Data/Thermo/tripletForces.txt $dir"
+copy1="log.lammps $dir"
 move2="Data/Thermo/thermo.txt $dir"
-copy1="../../lammps/src/pair_nn_angular2.cpp $dir"
-mv $move1
-mv $move2
+copy2="../../lammps/src/pair_nn_angular2.cpp $dir"
 cp $copy1
+mv $move2
+cp $copy2
 
 
 
